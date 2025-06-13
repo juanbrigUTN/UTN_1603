8bc3cfa3d60a1b3ecb2ceef07aee8d521a6ab6a0
juanbrig <juanmarcobrignole@.com>
2025-06-12 21:19:41 -0300

    parcialProgramacionTerminado

--- Juan/Programacion/Clases/5-6/ejercicioPosiciones&Max.c
+++ Juan/Programacion/Clases/5-6/ejercicioPosiciones&Max.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
 #include <time.h>
 #define p printf
@@ -7,25 +8,36 @@
 int main(void)
 {
     setlocale(LC_ALL, "spanish");
-    int i, num, max, acu, pos, cont, cant, flag=1;
+    int i, num = 0, max, acu = 0, pos, cont, cant = 1, flag1 = 1, flag2, suma = 0;
     srand(time(NULL));
-
-    p("Ingrese la cantidad de lotes: ");
-    s("\n%d",&cont);
-
-    for (int j=1 ; j<=cont ; j++)
+    do
     {
+        p("Ingrese la cantidad de lotes: ");
+        s("%d",&cont);
+        if (cont<=0)
+            p("Por favor, ingrese un número de lotes válido...\n\n");
+    }
+    while (cont<=0);
+
+    for (int j=1 ; j<= cont ; j++)
+    {
+        flag2 = 1;
+        if (flag1 == 1)
+        {
+            p("Ingrese la cantidad de números por lote: ");
+            s("%d",&cant);
+            p("\n\n\n");
+            flag1 = 0;
+        }
         for (i=1 ; i <= cant ; i++)
         {
-            acu = 0;
-            if (flag == 1)
+            if (flag2 == 1)
             {
-                p("Ingrese la cantidad de números por lote");
-                s("%d",&cant);
-                flag = 0;
+                acu = 0;
+                flag2 = 0;
             }
-            num =(rand()%(100))+1;//Iniciar semilla
-            acu+=num;
+            num = (rand()%(100))+1;
+            acu += num;
             p("%d\n",num);
             if(i==1)
             {
@@ -38,14 +50,14 @@ int main(void)
                 pos=i;
             }
         }
-        p("\nLa suma de todos los valores es: %d", acu);
-        p("\nEl valor máximo ingresado fue: %d", max);
-        p("\nLa posicición del valor max es: %d", pos);
+        p("--- Lote nro %d ---", j);
+        p("\nSuma de valores: %d", acu);
+        p("\nMáximo valor: %d (posición %d)\n\n\n", max, pos);
+
+        suma += acu;
     }
+    p("\n=============================\n");
+    p("\nSuma total de todos los lotes: %d", suma);
+
     return(0);
 }
-//    int num, num1;
-//    num=rand()%(10);
-//    num1=rand() % 10;
-//    p("%d este es num",num);
-//    p("%d este es 1",num1);
